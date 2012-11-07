fc68b82a3eb7965a8fc877bed3b1718bddb03713
paul.bilnoski <bilnoski@tamu.edu>
2012-11-07 15:49:32 -0600

    Add org.eclipse.rap.rwt.serverURL system property

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/util/URLHelper.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/rap/rwt/internal/util/URLHelper.java
@@ -82,6 +82,11 @@ public final class URLHelper {
   }
 
   private static String getServerURL() {
+    //[ariddle] - override for proxy compatibility
+    String serverPrefix = System.getProperty( "org.eclipse.rap.rwt.serverURL" );
+    if (serverPrefix != null) {
+      return serverPrefix;
+    }
     // TODO: [fappel] remove the creation of absolute addresses with
     //                relative ones, this should make this method obsolete
     HttpServletRequest request = ContextProvider.getRequest();
@@ -105,7 +110,7 @@ public final class URLHelper {
       StringBuilder buffer = new StringBuilder();
       buffer.append( ":" );
       buffer.append( result );
-      result = buffer.toString(); 
+      result = buffer.toString();
     } else {
       result = "";
     }
