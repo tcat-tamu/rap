961d5f2746ea98163891e47310c7793fa1c8d44b
Paul Bilnoski <bilnoski@tamu.edu>
2012-11-07 15:51:09 -0600

    Fix for decoration display context out

--- bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Decorations.java
+++ bundles/org.eclipse.rap.rwt/src/org/eclipse/swt/widgets/Decorations.java
@@ -42,6 +42,10 @@ public class Decorations extends Canvas {
   Decorations( Composite parent ) {
     // prevent instantiation from outside this package
     super( parent );
+    //[ariddle] - added for context issue fix
+    if (this.display == null) {
+      this.display = Display.getCurrent();
+    }
     images = new Image[0];
     text = "";
   }
